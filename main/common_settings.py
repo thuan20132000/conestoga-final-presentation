6eb659e2c8951f16c913a1be44dafc155ab6e12b
thuantm <thuantm@flydinotech.com>
2025-11-30 11:49:16 -0500

    - update

--- main/common_settings.py
+++ main/common_settings.py
@@ -211,6 +211,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.2.170:3000",  # FastAPI dev origin
     "http://192.168.2.170:3001",  # Django dev origin
     "https://develop.d3lgvc3ld1a6bg.amplifyapp.com",
+    "https://master.d2cve25ion05t3.amplifyapp.com",  # Django dev origin
     
 
 ]

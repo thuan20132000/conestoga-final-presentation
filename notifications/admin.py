34fa2f4f4c212fd2f398f4328e5c5b73a04d2691
thuantm <thuantm@flydinotech.com>
2026-01-30 23:18:50 -0500

    - add send new feature notification

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from webpush.models import PushInformation, Group, SubscriptionInfo
 from .models import Notification
+from webpush.utils import _send_notification
+import json
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
@@ -22,3 +24,30 @@ class SubscriptionInfoAdmin(admin.ModelAdmin):
     list_display = ("id", "endpoint", "auth", "p256dh", "browser", "user_agent")
     list_filter = ("browser",)
     search_fields = ("endpoint",)
+    
+
+# Unregister the default PushInformation admin and register our custom one
+admin.site.unregister(PushInformation)
+
+@admin.register(PushInformation)
+class PushInformationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "subscription", "group")
+    list_filter = ("group",)
+    search_fields = ("user__email", "user__username")
+    raw_id_fields = ("user", "subscription")
+    
+    actions = ["send_notification"]
+
+    def send_notification(self, request, queryset):
+        payload = {
+            "title": "🔔 New Feature Added", 
+            "body": "You can now edit your time entry and clock in/out from the app. Please reload your app to the latest version."
+        }
+        for device in queryset:
+            notification = _send_notification(device.subscription, json.dumps(payload), 0)
+            if notification:
+                self.message_user(request, "Notification sent successfully")
+            else:
+                self.message_user(request, "Notification failed to send")
+    send_notification.short_description = "Send Notification"
+    send_notification.allowed_permissions = ("change",)
